4fa824777ace997582f7808b84a64459af05079c
trillobite <jparnell0@protonmail.com>
2019-01-16 17:50:27 -0800

    gathers locations of all images, ready to start displaying them.

--- scripts/procedures/genConfig.js
+++ scripts/procedures/genConfig.js
@@ -49,13 +49,14 @@ define([], () => {
             let getDirs = (arr) => {
                 let dirArr = [];
                 arr.map((obj) => {
-                    let indxL = obj.indexOf("('") + 1;
+                    let indxL = obj.indexOf("(") + 2;
                     let indxR = obj.indexOf(")") - 1;
                     let extracted = obj.substring(indxL, indxR);
                     dirArr.push(extracted);
                 });
                 return dirArr;
             };
+
             let fetchImgArr = (obj) => {
                 let dfd = $.Deferred();
                 fetch(obj)
@@ -68,20 +69,49 @@ define([], () => {
                         let indxL = tmp.indexOf("[") + 1; //we don't want to include the bracket.
                         let indxR = tmp.indexOf("]") - 1; //we don't want to include the bracket.
                         tmp = tmp.substring(indxL, indxR);
-                        let imgArr = tmp.split(",");
+                        let imgLst = "";
+                        tmp.split('"').map((fragment) => { //get rid of the quotes.
+                            imgLst += fragment; //combine the fragments....
+                        });
+                        let imgArr = imgLst.split(",");
+                        //console.log(imgArr);
                         dfd.resolve(imgArr);
                     })
 
                 return dfd.promise();
             }
-            let getImgArr = (arr) => {
-                let jsonArr = [];
-                arr.map((obj) => {
-                    if(obj) {
 
+            let getImgArr = (arr) => {
+                let imgsArr = [];
+
+                let recursive = (obj) => {
+                    fetchImgArr(obj).done((imgArr) => {
+                        let nwImgArr = [];
+                        imgArr.map((imgNmStr) => {
+                            let dirImgStr = obj.substring(0, obj.indexOf("zmEvent"))
+                            nwImgArr.push(dirImgStr + imgNmStr);
+                        });
+                        imgsArr.push(nwImgArr);
+                        call(); //now that were done, call for another.
+                    });
+                };
+
+                let call = () => {
+                    let val = arr.shift();
+                    if(val) { //don't do anything if there is no value.
+                        recursive(val);
+                    } else {
+                        if(arr.length) {
+                            call(); //call again if objects are left.
+                        }
                     }
-                })
-            }
+                };
+
+                call(); //it would help if I would call the function that starts the recursive loop... this took me 1.5hrs... why won't the loop start... i thought... omg...
+                //console.log("imgsArr:", imgsArr);
+
+                return imgsArr;
+            };
 
             fetch('zmEventImagesMaster_20190108_112132.html')
                 .then(response => response.text())
@@ -90,14 +120,16 @@ define([], () => {
                     tmp = tmp.substring(0, tmp.indexOf("<div class='tab_content'") - 8);
                     tmp = tmp.split("<div>");
                     tmp = clean(tmp);
-                    console.log(getDirs(tmp));
-                    dfd.resolve(getDirs(tmp));
+                    let dirArr = getDirs(tmp);
+                    let imgArr = getImgArr(dirArr);
+                    console.log("done:", imgArr);
+                    dfd.resolve(imgArr);
                 });
-            
+
             return dfd.promise();
         };
 
-        
+
 
     };
 
